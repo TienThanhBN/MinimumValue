3d9e8aa4ccf392f7f8d8470fe6ddfc313f14bad0
TienThanhBN <tienthanhbn2006@gmail.com>
2024-12-06 15:20:29 +0700

    array

---
+++ src/MinimumValue.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MinimumValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số phần tử của mảng: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Nhập kích thước mảng");
+            return;
+        }
+
+        int[] array = new int[n];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Phần tử thứ " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        int min = array[0];
+        for (int i = 1; i < n; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Giá trị nhỏ nhất trong mảng là: " + min);
+    }
+}
